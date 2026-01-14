51bc7edd5ecd18ae42c55c155fd387de2eca03dc
MiltonRafael <miltonrafael166@gmail.com>
2026-01-14 12:58:26 -0300

    Simple query using query methods

--- src/main/java/com/miltonrafael/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/miltonrafael/workshopmongo/repository/PostRepository.java
@@ -1,9 +1,13 @@
 package com.miltonrafael.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.miltonrafael.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
